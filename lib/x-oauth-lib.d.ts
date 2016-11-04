41da71b584fe47da71b372d83bc4c705299beeb6
mikemichaelis <mike@xsdconsulting.com>
2016-11-04 11:02:05 -0400

    update

---
+++ lib/x-oauth-lib.d.ts
@@ -0,0 +1,5 @@
+import { OAuthService, OAUTHSERVICE_CONFIG_TOKEN } from './services/oauth-service';
+export { OAUTHSERVICE_CONFIG_TOKEN, OAuthService };
+export declare class XNG2OAuthLibModule {
+}
+export default XNG2OAuthLibModule;
